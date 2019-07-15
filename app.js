35361ff75558090ca5c95f8db3f8658754df5ab5
Michael Frantz <mjfrantz26@gmail.com>
2019-07-15 20:52:34 +0700

    Added env file

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) Middleware 
-app.use(morgan('tiny'));
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
